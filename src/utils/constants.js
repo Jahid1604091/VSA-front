402488208f8163a8171f8f28bc07f4e120528a9b
Jahid1604091 <jh409780@gmail.com>
2022-09-28 15:02:56 +0600

    updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const url = `http://localhost:5000/api`
-// export const url = `https://bloging-backend.up.railway.app/api`
+// export const url = `http://localhost:5000/api`
+export const url = `https://vsa-back-production.up.railway.app/api`
